6fe356ea926256ebc2ab82edd7be88c26a7ef13d
Chernenko-Valery <v02055020v@gmail.com>
2019-08-19 14:18:44 +0300

    Add AsyncTask

---
+++ Projects/AsyncTask/app/src/main/java/com/example/asynctask/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.asynctask;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
