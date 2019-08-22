be8db57d927fcd6bbf417f71bfffdd3e0ddcdbc0
Chernenko-Valery <v02055020v@gmail.com>
2019-08-22 12:04:54 +0300

    Add content Provider Example with querry and Insert

--- Projects/ContentProvider/app/src/main/java/com/example/contentprovider/PeopleContract.java
+++ Projects/ContentProvider/app/src/main/java/com/example/contentprovider/PeopleContract.java
@@ -1,8 +1,17 @@
 package com.example.contentprovider;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class PeopleContract {
+
+    public static final String CONTENT_AUTHORITY = "com.example.contentprovider";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_PEOPLE = "people";
+
+    public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PEOPLE);
 
     public static final class PeopleEntry implements BaseColumns {
         public final static String TABLE_NAME = "people";
--- Projects/ContentProvider/app/src/main/java/com/example/contentprovider/PeopleProvider.java
+++ Projects/ContentProvider/app/src/main/java/com/example/contentprovider/PeopleProvider.java
@@ -1,19 +1,51 @@
 package com.example.contentprovider;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.CancellationSignal;
 
 public class PeopleProvider extends ContentProvider {
+
+    private static final String PEOPLE_PROVIDER_LOG = "peopleProvider";
+
+    private static final int PEOPLE = 100;
+    private static final int PEOPLE_ID = 101;
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sUriMatcher.addURI(PeopleContract.CONTENT_AUTHORITY, PeopleContract.PATH_PEOPLE, PEOPLE);
+        sUriMatcher.addURI(PeopleContract.CONTENT_AUTHORITY, PeopleContract.PATH_PEOPLE + "/#", PEOPLE_ID);
+    }
+
+    private PeopleDBHelper mDBHelper;
+
     @Override
     public boolean onCreate() {
+        mDBHelper = new PeopleDBHelper(getContext());
         return false;
     }
 
     @Override
-    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        SQLiteDatabase database = mDBHelper.getReadableDatabase();
+        Cursor cursor;
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PEOPLE:
+                cursor = database.query(PeopleContract.PeopleEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+                return cursor;
+            case PEOPLE_ID:
+                selection = PeopleContract.PeopleEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                cursor = database.query(PeopleContract.PeopleEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+                return cursor;
+        }
         return null;
     }
 
@@ -24,7 +56,19 @@ public class PeopleProvider extends ContentProvider {
 
     @Override
     public Uri insert(Uri uri, ContentValues contentValues) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PEOPLE:
+                return insertPeople(uri, contentValues);
+            default:
+                return null;
+        }
+    }
+
+    private Uri insertPeople(Uri uri, ContentValues contentValues) {
+        SQLiteDatabase database = mDBHelper.getWritableDatabase();
+        long id = database.insert(PeopleContract.PeopleEntry.TABLE_NAME, null, contentValues);
+        return ContentUris.withAppendedId(uri, id);
     }
 
     @Override
